bf876f5e06e7b84af6bec2b9fa85376beb06c15d
PauRF03 <pauramos2003@gmail.com>
2024-11-11 11:09:42 +0100

    click on film

--- script.js
+++ script.js
@@ -1,25 +1,35 @@
-function filmClicked(){
-    let query = $("#search").val().toString();
-    if(query !== ""){
-        query = query.replace(" ", "_")
-        fetch(`http://www.omdbapi.com/?t=${query}&plot=full&apikey=1140ed56`)
-        .then(response => {
-            if (!response.ok) {
-              throw new Error('Error');
-            }
-            return response.json();
-          })
-          .then(data => {
-            if(data["Response"] === "True"){
-              $(".container").html(
-                `<div id = "image-ratings">
+function filmClicked() {
+  let query = $("#search").val().toString();
+  if (query !== "") {
+    query = query.replace(" ", "_")
+
+  }
+}
+
+function init() {
+  $("#searchBtn").click(searchClicked);
+  $("#search").on('keypress', function (e) {
+    if (e.keyCode == 13) searchClicked();
+  });
+  const imdbID = new URLSearchParams(window.location.search).get('f');
+  fetch(`http://www.omdbapi.com/?i=${imdbID}&plot=full&apikey=1140ed56`)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Error');
+      }
+      return response.json();
+    })
+    .then(data => {
+      if (data["Response"] === "True") {
+        $(".container").html(
+          `<div id = "image-ratings">
                 
                 </div>
                 <div id = "details">
                 
                 </div>`
-              )
-              $("#details").html(`
+        )
+        $("#details").html(`
                 <div class="data-container">
                   <h3 id="title">${data["Title"]}</h3>
                   <h4 id="director">&nbsp directed by &nbsp <strong>${data["Director"]}</strong></h4>
@@ -41,37 +51,38 @@ function filmClicked(){
                   <h6 id="earnings">Earnings: ${data["BoxOffice"]}</h6>
                 </div>
               `);
-              let s = `
+        let s = `
                 <img id="poster" src="${data["Poster"]}" alt="${data["Title"]} poster">
                 <table id="ratings">`;
-              if(data["Ratings"][0] != null){
-                s += `<tr>
+        if (data["Ratings"][0] != null) {
+          s += `<tr>
                     <td><a href="https://www.imdb.com/title/${data["imdbID"]}" target="_blank"><img class="logo" src="/media/imdb.svg" alt=""></a></td>
                     <td class="rating">${data["Ratings"][0]["Value"]} (${data["imdbVotes"]}  votes)</td>
-                  </tr>`;      
-              }
-              if(data["Ratings"][1] != null){
-                s += `<tr>
+                  </tr>`;
+        }
+        if (data["Ratings"][1] != null) {
+          s += `<tr>
                     <td><a href="https://www.rottentomatoes.com/m/${query}" target="_blank"><img class="logo" src="/media/rotten_tomatoes.svg" alt=""></a></td>
                     <td class="rating">${data["Ratings"][1]["Value"]}</td>
-                  </tr>`;      
-              }   
-              if(data["Ratings"][2] != null){
-                s+= `<tr>
+                  </tr>`;
+        }
+        if (data["Ratings"][2] != null) {
+          s += `<tr>
                     <td><a href="https://www.metacritic.com/movie/${query.replace("_", "-")}" target="_blank"><img class="logo" src="/media/metacritic.svg" alt=""></a></td>
                     <td class="rating">${data["Ratings"][2]["Value"]} </td>
-                  </tr>`;      
-              }
-              s += `</table>`;
-              $("#image-ratings").html(s);
-            }else{
-              $("#image-ratings").html("");
-              $("#details").html("");
-              $(".container").html(`<h2 id="error">${data["Error"]}</h2>`);
-            }
-          })
-          .catch(error => {
-            console.error('Error:', error);
-          });
-    }
+                  </tr>`;
+        }
+        s += `</table>`;
+        $("#image-ratings").html(s);
+      } else {
+        $("#image-ratings").html("");
+        $("#details").html("");
+        $(".container").html(`<h2 id="error">${data["Error"]}</h2>`);
+      }
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
 }
+
+$(document).ready(init);
