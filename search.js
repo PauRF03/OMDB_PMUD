d1769d3d910e7ca97753d3dc124f859ed0cd4807
PauRF03 <pauramos2003@gmail.com>
2024-11-12 18:29:22 +0100

    implement internal storage

--- search.js
+++ search.js
@@ -1,6 +1,8 @@
 let page = 1;
 let maxPages = 1;
 let query = "";
+let films = [];
+localStorage["recentlyVisited"] = localStorage["recentlyVisited"] || JSON.stringify(films);
 
 function firstPage() {
     page = 1;
@@ -22,9 +24,32 @@ function prevPage() {
     loadResults();
 }
 
+function updateVisited(id, title, poster) {
+    films.push({
+        id,
+        title,
+        poster
+    });
+    if (films.length > 5) films = films.slice(1, 6);
+    localStorage["recentlyVisited"] = JSON.stringify(films);
+    initRecentlyVisited();
+}
+
+function initRecentlyVisited(){
+    let s = `<h3 class="text-light pb-2"><strong>Recent searches</strong></h3>`;
+    for(let i = films.length - 1; i >= 0; i--){
+        s += `<a class="card bg-light col-2 ms-3" href="film.html?f=${films[i]["id"]}">
+                <img src="${films[i]["poster"]}" alt="${films[i]["title"]} poster" class="img-fluid p-2">
+                <h6 class="h6">${films[i]["title"]}</h4>
+            </div>
+        </a>`
+    }
+    $("#recentlyVisited").html(s);
+}
+
 function loadResults() {
     if (query !== "") {
-        fetch(`http://www.omdbapi.com/?s=${query}&page=${page}&apikey=1140ed56`)
+        fetch(`http://www.omdbapi.com/?s=${query.replace(" ", "_")}&page=${page}&apikey=1140ed56`)
             .then(response => {
                 if (!response.ok) {
                     throw new Error('Error');
@@ -32,9 +57,10 @@ function loadResults() {
                 return response.json();
             })
             .then(data => {
+                $("#recentlyVisited").html("");
                 maxPages = Math.ceil(data["totalResults"] / 10);
                 if (data["Response"] === "True") {
-                    let s = "";
+                    let s = `<h3 class="text-light pb-2"><strong>Results for "${query}"</strong></h3>`;
                     for (let i = 0; i < data["Search"].length; i++) {
                         s += `<div class="row card-fluid bg-light m-2 rounded align-self-center">`;
                         if (data["Search"][i]["Poster"] === "N/A") {
@@ -42,8 +68,7 @@ function loadResults() {
                         } else {
                             s += `<img class="col-2 rounded p-2 img-thumbnail img-fluid b-0" src=${data["Search"][i]["Poster"]} alt="${data["Search"][i]["Title"]} poster"</img>`
                         }
-                        s += `<a class="col-10 blockquote pl-4 pt-4" href="film.html?f=${data["Search"][i]["imdbID"]}" target="_blank"><h2> <strong>${data["Search"][i]["Title"]} (${data["Search"][i]["Year"]})</strong><br/>${data["Search"][i]["Type"]}</h2></a>
-                        </div>`;
+                        s += `<a class="col-10 blockquote pl-4 pt-4" href="film.html?f=${data["Search"][i]["imdbID"]}" target="_blank" onclick="updateVisited('${data["Search"][i]["imdbID"]}', '${data["Search"][i]["Title"]}', '${data["Search"][i]["Poster"]}')"><h2><strong>${data["Search"][i]["Title"]} (${data["Search"][i]["Year"]})</strong><br/>${data["Search"][i]["Type"]}</h2></a></div>`;
                     }
                     $("#searchResult").html(s);
                     let f = `<div class="row me-2 ms-2 mb-2 p-3" id="upper_container">
@@ -78,7 +103,7 @@ function loadResults() {
 function searchClicked() {
     page = 1;
     maxPages = 1;
-    query = $("#search").val().toString().replace(" ", "_");
+    query = $("#search").val().toString();
     loadResults();
 }
 
@@ -88,6 +113,8 @@ function init() {
     $("#search").on('keypress', function (e) {
         if (e.keyCode == 13) searchClicked();
     });
+    films = JSON.parse(localStorage["recentlyVisited"]);
+    initRecentlyVisited();
 }
 
 $(document).ready(init);
